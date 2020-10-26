14607b93319fc662a6cd040cf987abc39aaed941
tibssy <norbert.tiborcz@gmail.com>
2020-10-26 17:12:37 +0000

    Initial commit

--- engine.py
+++ engine.py
@@ -44,3 +44,24 @@ def slice_number(ones):
             res.append(segment_to_data(j))
 
     return np.asanyarray(res).astype(int)
+
+
+def detect(data):
+
+    def normalize(arr):
+        if len(arr.shape) == 1:
+            arr -= np.min(arr)
+            max_val = np.max(arr)
+        else:
+            arr -= np.min(arr, axis=1).reshape(arr.shape[0], 1)
+            max_val = np.max(arr, axis=1).reshape(arr.shape[0], 1)
+
+        return np.round(arr / max_val * 100).astype(int)
+
+    model = np.load('data.npy')
+
+    one = resize_image(data)
+    num = slice_number(one)
+
+    diff = np.sum(np.abs(normalize(model[:, 1:]) - normalize(num)), axis=1)
+    return np.array([model[np.argmin(diff), 0].astype(int), np.min(diff), np.round(100 - (np.min(diff) / np.max(diff) * 100)).astype(int)])
